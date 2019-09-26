356b5a154b0d62793489be8eedb33b91f891d46f
Isaac Sheets <isheets@lingoport.com>
2019-09-26 12:55:48 -0600

    added js

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+console.log("js loaded!");
+
+//reference to database
+var db = firebase.firestore();
+
+//add a student the to queue
+var addToQueue = () => {
+
+}
+
+//function to listen for changes
+var addToQueue = () => {
+
+}
+
+//function to update
+
